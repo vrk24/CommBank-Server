beb96043758da5633a10f5daa67cade5746975fd
vrk24 <vemularohithkumar24@gmail.com>
2026-06-05 01:14:24 +0530

    Add optional Icon field to Goal model

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -27,5 +27,5 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
-    public string? Icon { get; set; }
+    public string? Icon { get; set; } = "🎯";
 }
